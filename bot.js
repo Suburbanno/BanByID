58560c35d3ca52bc5e77cf5a8574dfd71fe20ae3
Vinicius <suburbanizar@gmail.com>
2023-01-14 11:10:40 -0300

    [feat] new commands and small improvements
    - /list - Returns the ids inside the .json file
    - /deleteid - Remove an id that is inside the .json file
    - /count - Returns the number of ids in the .json file

--- bot.js
+++ bot.js
@@ -10,29 +10,67 @@ try {
 } catch (error) {
   console.error(error);
   bannedUsers = [];
+  fs.writeFileSync('spam-users-id.json', JSON.stringify(bannedUsers));
 }
 
-bot.command('add', async (ctx) => {
-  const message = ctx.message;
-  const chat = await ctx.telegram.getChat(message.chat.id);
-  const chatMember = await ctx.telegram.getChatMember(message.chat.id, message.from.id);
-
-  if (!message.from.is_bot && (chat.creator === message.from.id || chatMember.status === 'administrator')) {
-    bannedUsers.push(message.reply_to_message.from.id);
-    fs.writeFileSync('spam-users-id.json', JSON.stringify(bannedUsers));
-    ctx.reply(`O usuário ${message.reply_to_message.from.first_name} foi adicionado à lista de usuários marcados como Spam/Bot.`);
+bot.command('add', (ctx) => {
+  if (ctx.from.id === xxxxxxxx) {
+    const message = ctx.message;
+    const userId = message.reply_to_message.from.id;
+    if (!bannedUsers.includes(userId)) {
+      ctx.telegram.KickChatMember(message.chat.id, userId)
+      bannedUsers.push(userId);
+      fs.writeFileSync('spam-users-id.json', JSON.stringify(bannedUsers));
+      ctx.reply(`O usuário ${message.reply_to_message.from.first_name} foi adicionado à lista de usuários marcados como Spam/Bot e foi banido do grupo.`);
+    } else {
+      ctx.reply(`O usuário ${message.reply_to_message.from.first_name} já está na lista de usuários marcados como Spam/Bot.`);
+    }
   } else {
-    ctx.reply('Você não tem permissão para adicionar usuários à lista de usuários banidos.');
+    ctx.reply('Você não tem permissão para adicionar usuários na lista de usuários banidos.');
+  }
+});
+
+
+bot.command('deleteid', (ctx) => {
+  if (ctx.from.id === 668557709) {
+    const message = ctx.message;
+    const userId = message.reply_to_message.from.id;
+    const index = bannedUsers.indexOf(userId);
+    if (index > -1) {
+      bannedUsers.splice(index, 1);
+      fs.writeFileSync('spam-users-id.json', JSON.stringify(bannedUsers));
+      ctx.reply(`O usuário ${message.reply_to_message.from.first_name} foi removido da lista de usuários marcados como Spam/Bot.`);
+    } else {
+      ctx.reply(`O usuário ${message.reply_to_message.from.first_name} não está na lista de usuários marcados como Spam/Bot.`);
+    }
+  } else {
+    ctx.reply('Você não tem permissão para remover usuários da lista de usuários banidos.');
+  }
+});
+
+bot.command('count', (ctx) => {
+  ctx.reply(`Existem ${bannedUsers.length} usuários na lista de usuários marcados como Spam/Bot.`);
+});
+
+bot.command('list', (ctx) => {
+  if (bannedUsers.length > 0) {
+      ctx.reply(`Lista de usuários marcados como Spam/Bot: ${bannedUsers.join(', ')}`);
+  } else {
+      ctx.reply(`Não há usuários marcados como Spam/Bot na lista.`);
   }
 });
 
 bot.on('new_chat_members', (ctx) => {
   const userId = ctx.message.new_chat_participant.id;
   if (bannedUsers.includes(userId)) {
-    bot.telegram.kickChatMember(ctx.message.chat.id, userId);
+    ctx.telegram.kickChatMember(ctx.message.chat.id, userId);
     const userName = ctx.message.new_chat_participant.first_name;
-    ctx.reply(` O úsuario ${userName}, foi banido pois está marcado como Spam/Bot. `);
+    ctx.reply(` O usuário ${userName}, foi banido pois está marcado como Spam/Bot. `);
   }
+});
+
+bot.catch((err) => {
+    console.log('Error:', err)
 });
 
 const startBot = async () => {
